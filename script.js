4c54e1c67551c89470328b28b5e2538fe462ba36
msadza <60848941+sadzamzeko@users.noreply.github.com>
2024-07-27 03:10:26 +0400

    feat: add responsive

--- script.js
+++ script.js
@@ -13,30 +13,36 @@ const cards2Data = () => {
 
 document.addEventListener('DOMContentLoaded', () => {
     cards2Data().then(jsonData => {
-        generateCardsHTML(jsonData);
+        generateCards2HTML(jsonData);
     });
     faqAnswer();
     burgerMenu();
  
 });
 
-function generateCardsHTML(data) {
+function generateCards2HTML(data) {
     let html = '';
     const container = document.getElementById('cards2-container');
 
     data.forEach(item => {
         html += `
             <div class="card2">
-                <img src="img/img/Icon Button.png" alt="arrowOne" class="arrowOne" id="arrowOne">
-                <img src="${item.imageURL}" alt="${item.name}">
+                <img src="img/img/Icon Button.svg" alt="arrowOne" class="arrowOne" id="arrowOne">
+                <img src="${item.imageURL}" alt="${item.name}" class="cardIMg">
                 <img src="img/img/Frame 92.png" alt="arrowW" class="arrowW" id="arrowW">
                 <div class="card2-content">
-                    <h4>${item.name}</h4>
+                    <h4 class="allHomeName">${item.name}</h4>
                     <p class="titleDesk">${item.desc}</p>
-                    <span class="priceText">${item.price}</span>
-                    <span>₾</span><br/>
-                    <button type="button" class="cartButton">კალათაში დამატება</button">
-               
+                    <span class="priceText">${item.price}</span>&nbsp;&nbsp;
+                   <span class="allHomeStartPr">${item.startPrice}</span>&nbsp;
+                   ${
+                    item.discount
+                      ? `<span class="allHomeDisc">${item.discount}</span>`
+                      : ""
+                  }
+                   
+                    <br/>
+                    <button type="button"  class="cartButton">კალათაში დამატება</button">
                 </div>
             </div>
         `;
